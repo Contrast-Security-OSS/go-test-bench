7e25bdd7ed67a8dda4435b25b595a81ff59299dc
James Roberts <james.roberts@contrastsecurity.com>
2022-01-04 11:15:26 -0500

    Unexport defaultAddr and add comment

--- cmd/exercise/main.go
+++ cmd/exercise/main.go
@@ -5,11 +5,12 @@ import (
 	"log"
 )
 
-const DefaultAddr = "localhost:8080"
+// defaultAddr is the default address to send requests to.
+const defaultAddr = "localhost:8080"
 
 func main() {
 	log.Println("Starting...")
-	addr := flag.String("addr", DefaultAddr, "`host:port` to access the listening server")
+	addr := flag.String("addr", defaultAddr, "`host:port` to access the listening server")
 	flag.Parse()
 	if err := exercise(*addr); err != nil {
 		log.Fatal(err)
