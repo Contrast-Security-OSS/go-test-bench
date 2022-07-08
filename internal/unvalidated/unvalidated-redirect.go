b76b432a3bc218c6c22ab1ca8a6738a5e5dfcc1e
James Roberts <james.roberts@contrastsecurity.com>
2022-07-08 14:14:28 -0400

    Add doc comment

--- internal/unvalidated/unvalidated-redirect.go
+++ internal/unvalidated/unvalidated-redirect.go
@@ -4,6 +4,7 @@ import (
 	"github.com/Contrast-Security-OSS/go-test-bench/internal/common"
 )
 
+// RegisterRoutes is to be called to add the routes in this package to common.AllRoutes.
 func RegisterRoutes(frameworkSinks ...*common.Sink) {
 	sinks := frameworkSinks
 	common.Register(common.Route{
